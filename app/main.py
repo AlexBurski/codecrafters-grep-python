321cb8eb18835a74f12223e11892fd6fb026150c
Alexander Burski <bssb.sbbs@mail.ru>
2024-09-11 22:04:32 -0400

    Solve Start of string anchor. Refactor

--- app/main.py
+++ app/main.py
@@ -5,53 +5,44 @@ import sys
 
 
 def match_pattern(input_line, pattern):
+    if pattern.startswith("^"):
+        return string_search(pattern[1:], input_line, start_index=0)
     for i in range(len(input_line)):
-        if recursive_search(pattern, input_line[i:], start_index = i):
+        if string_search(pattern, input_line[i:], start_index=i):
             return True
     return False
-def positive_char_from_group(group, input_line):
-    start_index = group.find("[")
-    end_index = group.find("]")
 
-    if start_index == -1 or end_index == -1 or start_index > end_index:
-        raise ValueError("Pattern must contain characters between '[' and ']'")
-    character_set = set(group[start_index + 1: end_index])
-    input_line_set = set(input_line)
 
-    if start_index + 1 == group.find("^"):
-        return not bool(character_set & input_line_set)
-
-    return bool(character_set & input_line_set)
-
-def recursive_search(pattern, input_line, start_index):
+def string_search(pattern, input_line, start_index):
     pattern_index = 0
     input_index = start_index
 
     while pattern_index < len(pattern) and input_index < len(input_line):
-        if pattern[pattern_index:pattern_index + 2] == r"\d":
+        if pattern[pattern_index : pattern_index + 2] == r"\d":
             if not input_line[input_index].isdigit():
                 return False
             pattern_index += 2
             input_index += 1
-        elif pattern[pattern_index:pattern_index + 2] == r"\w":
+        elif pattern[pattern_index : pattern_index + 2] == r"\w":
             if not input_line[input_index].isalnum():
                 return False
             pattern_index += 2
             input_index += 1
 
-        elif pattern[pattern_index] == '[':
-            closing_bracket_index = pattern.find(']', pattern_index)
+        elif pattern[pattern_index] == "[":
+            closing_bracket_index = pattern.find("]", pattern_index)
             if closing_bracket_index == -1:
                 raise ValueError("Unmatched '[' in pattern")
 
-            char_set = pattern[pattern_index + 1:closing_bracket_index]
-            is_negated = char_set.startswith('^')
+            char_set = pattern[pattern_index + 1 : closing_bracket_index]
+            is_negated = char_set.startswith("^")
 
             if is_negated:
                 char_set = char_set[1:]
 
             if (is_negated and input_line[input_index] in char_set) or (
-                    not is_negated and input_line[input_index] not in char_set):
+                not is_negated and input_line[input_index] not in char_set
+            ):
                 return False
 
             pattern_index = closing_bracket_index + 1
@@ -65,6 +56,7 @@ def recursive_search(pattern, input_line, start_index):
 
     return pattern_index == len(pattern)
 
+
 def main():
     pattern = sys.argv[2]
     input_line = sys.stdin.read()
@@ -72,7 +64,6 @@ def main():
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
-
 
     # Uncomment this block to pass the first stage
     if match_pattern(input_line, pattern):
